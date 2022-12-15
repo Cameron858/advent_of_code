68c3f0bd05e1b4726954bd245cc581f8cf53a199
Cameron Wood <Cameron.Wood@tessella.com>
2022-12-15 11:44:39 +0000

    Working day 14 solutions.

--- 22/python/day_14.py
+++ 22/python/day_14.py
@@ -1,10 +1,11 @@
 from copy import deepcopy
+import math
 
 
 def load_input():
     """Load in the data."""
     try:
-        with open(r"22\data\day_14_example_1.txt") as file_input:
+        with open(r"22\data\day_14.txt") as file_input:
             input_lines = file_input.readlines()
     except OSError as e:
         print(f"Error {e} raised.")
@@ -71,7 +72,7 @@ def part_1(data: list[str]):
     
     # draw on rocks
     rock_positions_list = set()
-    scan[0][500 - min_col] = '+'
+    scan[0][sand_source_column - min_col] = '+'
     for row in data:
         for p1, p2 in swindow(row):
             rock_positions = interpolate_between_two_points(p1, p2)
@@ -84,12 +85,11 @@ def part_1(data: list[str]):
     display_scan(scan)
 
     # simulate sand
-    fallen_sand_positions = set()
     unit = 0
     while True:
         print("Spawning sand...", unit)
 
-        initial_position = (500, 1)
+        initial_position = (sand_source_column, 1)
         x, y = initial_position
         x -= min_col
 
@@ -99,6 +99,108 @@ def part_1(data: list[str]):
     
                 temp_scan = deepcopy(scan)
                 temp_scan[y][x] = 'O'
+                display_scan(temp_scan)
+
+                if scan[y + 1][x] not in ['#', 'O']:
+                    # print(f"Moving down.")
+                    y += 1
+                else:
+                    # print("The path below is blocked. Check diagonals")
+                    # bottom left
+                    if scan[y + 1][x - 1] not in ['#', 'O']:
+                        # print("Bottom left is free.")
+                        y += 1
+                        x -= 1
+                        continue
+                    
+                    if scan[y + 1][x + 1] not in ['#', 'O']:
+                        y += 1
+                        x += 1
+                        # print("Bottom right is free")
+                        continue
+                    
+                    # print(f"This unit cannot move.")
+                    scan[y][x] = 'O'
+                    # fallen_sand_positions.add((x, y))
+                    moving = False
+        except IndexError:
+            print(f"Unit {unit} has fallen into the abyss!")
+            break
+        
+        unit += 1
+    
+    print(f"{unit} units of sand fell.")
+
+
+def find_pyramid_base_width(height: int):
+    # 1 -> 1
+    # 2 -> 3
+    # 3 -> 5
+    # 4 -> 7
+    return 2 * (height) - 1
+
+
+def part_2(data):
+
+    sand_source_column = 500
+    data: list[list[tuple[int, int]]] = [[(int(d.split(",")[0]), int(d.split(",")[1])) for d in row.strip().split(" -> ")] for row in data]
+
+    column_numbers = []
+    row_numbers = []
+    for row in data:
+        for point in row:
+            column_numbers.append(point[0])
+            row_numbers.append(point[1])
+    
+    min_col, max_col = min(column_numbers), max(column_numbers)
+    min_row, max_row = min(row_numbers), max(row_numbers)
+    print(f"Column range: {min_col} to {max_col}\nRow range: {min_row} to {max_row}")
+    print(f"The scan has {max_row + 1} rows and {max_col - min_col + 1} columns")
+    inf_floor_width = find_pyramid_base_width(max_row + 2)
+    print(f"The inf floor must be {inf_floor_width} wide")
+    print(f"{(inf_floor_width - 1) // 2} must be added either side of the sand source.")
+
+    # padding for inf floor
+    l_dist = sand_source_column - min_col
+    r_dist = (max_col - min_col) - l_dist
+    print(f"The source is {l_dist} away from the left and {r_dist} away from the right")
+    left_padding = ((inf_floor_width - 1) // 2) - l_dist
+    right_padding =  ((inf_floor_width - 1) // 2) - r_dist + 1
+    print(f"{left_padding=}, {right_padding=}")
+
+    # create empty scan
+    scan = [['.' for _ in range(max_col - min_col + 1)] for _ in range(max_row + 1)]  
+    
+    # draw on rocks
+    rock_positions_list = set()
+    scan[0][sand_source_column - min_col] = '+'
+    for row in data:
+        for p1, p2 in swindow(row):
+            rock_positions = interpolate_between_two_points(p1, p2)
+            for rp in rock_positions:
+                x, y = rp
+                # print(f"Drawing {x=} and {y=} @ x={x - min(column_numbers)} and y={y}")
+                rock_positions_list.add((y, x - min_col))
+                scan[y][x - min_col] = '#'
+
+    # add inf floor to scan
+    scan = [['.' for _ in range(left_padding)] + row + ['.' for _ in range(right_padding)] for row in scan]
+    scan.append(['.' for _ in range(len(scan[0]))])
+    scan.append(['#' for _ in range(len(scan[0]))])
+    # display_scan(scan)
+
+    # simulate sand
+    unit = 0
+    while scan[0][(sand_source_column - min_col) + left_padding] not in ['#', 'O']:
+        initial_position = (sand_source_column  + left_padding, 0)
+        x, y = initial_position
+        x -= min_col
+
+        try:
+            moving = True
+            while moving:
+                # temp_scan = deepcopy(scan)
+                # temp_scan[y][x] = 'O'
                 # display_scan(temp_scan)
 
                 if scan[y + 1][x] not in ['#', 'O']:
@@ -134,4 +236,4 @@ def part_1(data: list[str]):
 
 if __name__ == "__main__":
     data = load_input()
-    part_1(data)
+    part_2(data)
